0d53e0deba17db5772f938198ccef04eba9c3b37
devadg <opponamer@gmail.com>
2024-04-03 23:21:01 +0100

    changed font

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import Footer from "./components/Footer";
 import "./globals.css";
 import localFont from 'next/font/local';
 import { Inter, Poppins } from 'next/font/google'
-import Head from "next/head";
  
 // const anyFont = Poppins({
 //   subsets: ['latin'],
@@ -10,17 +9,17 @@ import Head from "next/head";
 //   weight: ["100", "200", "300"]
 // })
 
-const anyFont = localFont({
-  src: [
-    {
-      path: './fonts/Poppins-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-      variable: '--font-poppins',
-      fallback: ['system-ui', 'arial']
-    },
-  ],
-})
+// const anyFont = localFont({
+//   src: [
+//     {
+//       path: './fonts/Poppins-Regular.woff2',
+//       weight: '400',
+//       style: 'normal',
+//       variable: '--font-poppins',
+//       fallback: ['system-ui', 'arial']
+//     },
+//   ],
+// })
 
 export const metadata = {
   icons: {
@@ -90,9 +89,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={anyFont.className}>
-      <Head>
-      </Head>
+    <html lang="en" className="">
       <body className="">
           {children}
           <Footer />
